5122d78d74de19eaa50f4052ebf9e3b541ee9b77
Ming Li <ml2954@na.corp>
2025-04-03 11:36:42 -0600

    vpython compatiblity

--- main.py
+++ main.py
@@ -1,44 +1,50 @@
-###### Define Parameters #####
-G = 1
-M = 1
-R = 1
-rsoft = 0.03
-w = vector(0,0.05,0)
-n = 0
-N = 100
-stars = []
+import asyncio
+from vpython import sphere, vector, rate, mag, norm, cross
+import random
 
-##### Main #####
-while n<N:
-  rt = R*vector(2*random()-1,2*random()-1,2*random()-1)
-  stars = stars + [sphere(pos=rt,radius=R/30,make_trail=False,retain=100)]
-  n = n + 1
+async def main():
+    ###### Define Parameters #####
+    G = 1
+    M = 1
+    R = 1
+    rsoft = 0.03
+    w = vector(0, 0.05, 0)
+    n = 0
+    N = 100
+    stars = []
 
-for star in stars:
-  star.m = M/N
-  star.p = star.m*cross(w,vector(star.pos.x,0,star.pos.z))
-  star.F = vector(0,0,0)
+    ##### Main #####
+    while n < N:
+        rt = R * vector(2 * random.random() - 1, 2 * random.random() - 1, 2 * random.random() - 1)
+        stars.append(sphere(pos=rt, radius=R / 30, make_trail=False, retain=100))
+        n += 1
 
-t = 0
-dt = 0.01
+    for star in stars:
+        star.m = M / N
+        star.p = star.m * cross(w, vector(star.pos.x, 0, star.pos.z))
+        star.F = vector(0, 0, 0)
 
-while t<10:
-  rate(100)
-  for star in stars:
-    star.F = vector(0,0,0)
-  for i in range(len(stars)):
-    for j in range(len(stars)):
-      if i!=j:
-        rji = stars[i].pos - stars[j].pos
-        stars[i].F = stars[i].F - G*stars[i].m*stars[j].m*norm(rji)/(mag(rji)**2+rsoft**2)
-        
-  for star in stars:
-    star.p = star.p + star.F*dt
-    star.pos = star.pos + star.p*dt/star.m
-  t = t + dt
+    t = 0
+    dt = 0.01
 
-  ##### End of Python Codes #####
-        
-        
-        
-        
+    while t < 10:
+        rate(100)
+        for star in stars:
+            star.F = vector(0, 0, 0)
+        for i in range(len(stars)):
+            for j in range(len(stars)):
+                if i != j:
+                    rji = stars[i].pos - stars[j].pos
+                    stars[i].F -= G * stars[i].m * stars[j].m * norm(rji) / (mag(rji)**2 + rsoft**2)
+
+        for star in stars:
+            star.p += star.F * dt
+            star.pos += star.p * dt / star.m
+        t += dt
+
+# Run the main function using asyncio
+asyncio.run(main())
+
+
+
+
